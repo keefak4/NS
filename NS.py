921daf2c33d56a19b1d511a472565532c070dd69
Vladislav Russo <vladtirma@gmail.com>
2022-03-22 01:55:32 +0300

    L1
    Start

---
+++ NS.py
@@ -0,0 +1,12 @@
+widht = 0.1
+def neumar_word (input,widht):
+    prediction = input * widht
+    return prediction
+number_of_toys = [8.5,41.2,4,10]
+input = number_of_toys[0]
+pred = neumar_word(input,widht)
+print(pred)
+
+
+    
+    
